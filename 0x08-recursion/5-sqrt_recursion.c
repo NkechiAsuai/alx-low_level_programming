61aa06512ed1493999a33c0b5e978e46dda840b2
NkechiAsuai <nkayasuai01@gmail.com>
2023-04-27 06:18:39 -0700

    created 0x08-recursion folder

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int sqrt_a(int n, int i);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * @b: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (sqrt_a(n, 0));
+}
+
+/**
+ * sqrt_a  - main - check the code
+ * Return: resulting square root
+ */
+
+int sqrt_a(int n, int i)
+{
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (sqrt_a(n, i + 1));
+}
